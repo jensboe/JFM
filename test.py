6bd5e99cb0353004ac61de9e08b68cef7f91c0dd
Jens Böckmann <jensboe@outlook.de>
2022-12-08 22:54:34 +0100

    First testshot

---
+++ test.py
@@ -0,0 +1,24 @@
+import cv2
+from pathlib import Path
+
+# load image
+image = cv2.imread("input.jpg")
+
+
+# face detection
+modelpath = Path(cv2.__file__).parent / "data"
+frontalface = modelpath / "haarcascade_frontalface_default.xml"
+face_cascade = cv2.CascadeClassifier(str(frontalface))
+
+
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+faces = face_cascade.detectMultiScale(gray_image, 1.3, 5)
+
+# face crop
+for (x, y, w, h) in faces:
+    face_cropped = image[y:y+h, x:x+w]
+
+# show results
+cv2.imshow("Face Cropped", face_cropped)
+cv2.waitKey(0)
