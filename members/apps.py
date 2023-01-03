b48afa6798ec14a4b318814d3d8f3b36a10b54ad
Jens Böckmann <jensboe@outlook.de>
2023-01-03 22:45:15 +0100

    Add basic member model, view and templates

--- members/apps.py
+++ members/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MemberConfig(AppConfig):
+class MembersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'member'
+    name = 'members'
--- members/views.py
+++ members/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.views import generic
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .models import Member
+
+
+class ListView(generic.ListView):
+    model = Member
+    context_object_name = 'members'
+
+
+class DetailView(generic.DetailView):
+    model = Member
+
+
+class UpdateView(generic.UpdateView):
+    model = Member
+    success_url = reverse_lazy('members:list')
+    fields = ['firstname', 'lastname']
+
+
+class CreateView(generic.CreateView):
+    model = Member
+    success_url = reverse_lazy('members:list')
+    fields = ['firstname', 'lastname']
