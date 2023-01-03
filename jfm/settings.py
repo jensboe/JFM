ab75b2c3f35d37ca5042f96d9ac94a26efa90b77
Jens Böckmann <jensboe@outlook.de>
2023-01-03 22:45:15 +0100

    Add basic member model, view and templates

--- jfm/settings.py
+++ jfm/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'members.apps.MembersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- members/models.py
+++ members/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Member(models.Model):
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+
+    @property
+    def fullname(self) -> str:
+        return f'{self.firstname} {self.lastname}'
+
+    def __str__(self) -> str:
+        return self.fullname
