5a622d7570265fa203047cd8222839f37b5b5199
Jens Böckmann <jensboe@outlook.de>
2023-01-03 23:10:54 +0100

    Add image field

--- members/models.py
+++ members/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Member(models.Model):
     firstname = models.CharField(max_length=100)
     lastname = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='members', null=True, blank=True)
 
     @property
     def fullname(self) -> str:
