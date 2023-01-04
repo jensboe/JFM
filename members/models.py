5f187c4a9211d8a1b77536e80a9315d2a81721ce
Jens Böckmann <jensboe@outlook.de>
2023-01-04 15:48:30 +0100

    Add a create_squere function

--- members/models.py
+++ members/models.py
@@ -1,4 +1,9 @@
+import cv2
+from pathlib import Path
 from django.db import models
+from jfm.settings import MEDIA_ROOT
+from django.core.files.base import ContentFile
+import logging
 
 
 class Member(models.Model):
@@ -14,3 +19,34 @@ class Member(models.Model):
 
     def __str__(self) -> str:
         return self.fullname
+
+    def save(self, *args, **kwargs) -> None:
+        self._create_square()
+        return super().save(*args, **kwargs)
+
+    def _create_square(self) -> None:
+        store = MEDIA_ROOT / self.image.name
+        img = cv2.imread(str(store))
+
+        modelpath = Path(cv2.__file__).parent / 'data'
+
+        frontalface = modelpath / 'haarcascade_frontalface_default.xml'
+        face_cascade = cv2.CascadeClassifier(str(frontalface))
+        gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        faces = face_cascade.detectMultiScale(gray_image)
+        if len(faces) > 0:
+            (x, y, w, h) = faces[0]
+            # calculate the face center
+            yc = int(y + (h / 2))
+            xc = int(x + (w / 2))
+
+            size = min(yc, xc, (img.shape[1] - yc), (img.shape[0] - xc))
+
+            face_cropped = img[yc - size:yc + size, xc - size:xc + size]
+
+            ret, buf = cv2.imencode('.jpg', face_cropped)
+            self.image_square = ContentFile(
+                buf.tobytes(), name=f'{self.fullname}.jpg')
+        else:
+            logging.error(f'Detect {len(faces)} faces instead of one')
