c8f69925d266b991c2da6bf36562b498bead4d7c
Jens Böckmann <jensboe@outlook.de>
2023-01-05 13:30:15 +0100

    Add start and end date and times

--- events/models.py
+++ events/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 
+from django.utils import timezone
+
 
 class Event(models.Model):
     title = models.CharField(max_length=200)
+    start_date = models.DateField(default=timezone.now)
+    start_time = models.TimeField(default=timezone.now)
+    end_date = models.DateField(default=timezone.now)
+    end_time = models.TimeField(default=timezone.now)
 
     def __str__(self) -> str:
         return self.title
