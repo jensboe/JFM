28aa5dda7e809c71886d30657142e81eb807c2ca
Jens Böckmann <jensboe@outlook.de>
2023-01-05 12:45:25 +0100

    Add a detail view

--- events/urls.py
+++ events/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'events'
 urlpatterns = [
     path('', views.ListView.as_view(), name='list'),
-    # path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     # path('<int:pk>/update', views.UpdateView.as_view(), name='update'),
     # path('create/', views.CreateView.as_view(), name='create'),
 ]
--- events/views.py
+++ events/views.py
@@ -10,5 +10,9 @@ class ListView(generic.ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Events'
-        context['element_url'] = 'members:detail'
+        context['element_url'] = 'events:detail'
         return context
+
+
+class DetailView(generic.DetailView):
+    model = Event
