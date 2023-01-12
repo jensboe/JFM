19cf220b5c8f999e173cbc54ea80b836adc46c52
Jens <jensboe@outlook.de>
2023-01-12 21:52:02 +0100

    Jensboe/issue25 (#26)
    * Add a dev docker system
    * Setup depoyment with gunicorn
    
    fix #25

--- jfm/settings.py
+++ jfm/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,12 +26,13 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lic9s&a@*-w25ai50m($n4k^np3k0k--u$(&uz61o=g&^xqnxq'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = int(os.environ.get('DEBUG', default=0))
+ALLOWED_HOSTS = os.environ.get(
+    'DJANGO_ALLOWED_HOSTS',
+    default='localhost').split(' ')
 
 
 # Application definition
