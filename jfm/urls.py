bdf3007ab3aec3704584fbf2d3ca39373c7a8778
Jens <jensboe@outlook.de>
2023-01-16 00:11:03 +0100

    feat(login): Add loginpage

--- jfm/urls.py
+++ jfm/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('events/', include('events.urls')),
     path('admin/', admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
