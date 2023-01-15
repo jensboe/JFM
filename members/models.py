f980e3d0bd50c94de0c2043ee8a970b31f8f08ca
Jens Böckmann <jensboe@outlook.de>
2023-01-15 23:04:26 +0100

    Add new events to all members

--- members/models.py
+++ members/models.py
@@ -28,9 +28,16 @@ class Member(models.Model):
         return self.fullname
 
     def save(self, *args, **kwargs) -> None:
+        from events.models import Participant, Event
         self.image_square = self._create_headshoot_square(self.image)
         self.image_passport = self._create_headshoot(self.image, 7, 9)
-        return super().save(*args, **kwargs)
+        super().save(*args, **kwargs)
+        super().save(*args, **kwargs)
+        for event in Event.objects.all():
+            Participant.objects.update_or_create(
+                member=self,
+                event=event
+            )
 
     def _create_headshoot_square(
             self, src_img_field: models.ImageField) -> ContentFile:
