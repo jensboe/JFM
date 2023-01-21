68930198169233e8af999880c5800168fa60ef0a
Jens Böckmann <jensboe@outlook.de>
2023-01-21 14:28:10 +0100

    Update language

--- events/urls.py
+++ events/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.utils.translation import gettext_lazy as _
+from django.conf.urls.i18n import i18n_patterns
 
 from . import views
 from .feeds import EventFeed
@@ -10,14 +12,12 @@ urlpatterns = [
         '<int:pk>/',
         views.ParticipantFormView.as_view(),
         name='participation'),
-    path(
-        '<int:pk>/detail',
-        views.EventDetailView.as_view(),
-        name='detail'),
-    path(
-        '<int:pk>/update',
-        views.EventUpdateView.as_view(),
-        name='update'),
-    path('create/', views.EventCreateView.as_view(), name='create'),
-    path('feed/', EventFeed())
+    path(_('<int:pk>/detail/'),
+         views.EventDetailView.as_view(),
+         name='detail'),
+    path(_('<int:pk>/update/'),
+         views.EventUpdateView.as_view(),
+         name='update'),
+    path(_('create/'), views.EventCreateView.as_view(), name='create'),
+    path(_('feed/'), EventFeed())
 ]
