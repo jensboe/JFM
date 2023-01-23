d1d96903d28b5df1307f37091e97fe3d7d99078e
Jens <jensboe@outlook.de>
2023-01-23 22:41:25 +0100

    feat(members): Add  "is_instructor" field (#75)
    Instructor are listed below regular members.
    
    closes #59

--- events/apps.py
+++ events/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class EventsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'events'
+    verbose_name = _('events')
