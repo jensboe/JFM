9af7f0a49226ff324b9809e26dbdcdca6247010d
Jens Böckmann <jensboe@outlook.de>
2024-03-09 22:39:10 +0100

    Make material event lists

--- frontend/src/app/event/event.module.ts
+++ frontend/src/app/event/event.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule, NgFor } from '@angular/common';
 import { NewComponent } from './new/new.component';
 import { ListComponent } from './list/list.component';
+import { MaterialModule } from '../material/material.module';
 
 
 
 @NgModule({
   declarations: [
     NewComponent,
-    ListComponent
+    ListComponent,
   ],
   imports: [
     CommonModule,
-    NgFor
+    NgFor,
+    MaterialModule
   ],
   exports: [
     NewComponent,
