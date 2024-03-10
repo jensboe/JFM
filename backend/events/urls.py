638059fed2e5229592e5705f934165b2c69b1923
Jens Böckmann <jensboe@outlook.de>
2024-03-10 14:35:01 +0100

    Add events routing

--- backend/events/urls.py
+++ backend/events/urls.py
@@ -1,23 +1,16 @@
-from django.urls import path
+from django.urls import path, include
 from django.utils.translation import gettext_lazy as _
 from django.conf.urls.i18n import i18n_patterns
-
+from rest_framework import routers
 from . import views
 from .feeds import EventFeed
 
+
+router = routers.DefaultRouter()
+router.register(r'', views.EventViewSet)
+
 app_name = 'events'
 urlpatterns = [
-    path(
-        '<int:pk>/',
-        views.ParticipantFormView.as_view(),
-        name='participation'),
-    path('<int:pk>/detail/',
-         views.EventDetailView.as_view(),
-         name='detail'),
-    path('<int:pk>/update/',
-         views.EventUpdateView.as_view(),
-         name='update'),
-    path('create/', views.EventCreateView.as_view(), name='create'),
+    path ('', include(router.urls)),
     path('feed/calender.ics', EventFeed()),
-    path('', views.EventViewSet.as_view({'get': 'list'}))
 ]
