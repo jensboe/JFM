5e4a52bba0c7f49bb5cb82b1332be041c09f75ab
Jens Böckmann <jensboe@outlook.de>
2024-03-23 17:38:43 +0100

    Add upcomming and last event list view

--- frontend/src/app/event/list/list.component.ts
+++ frontend/src/app/event/list/list.component.ts
@@ -1,29 +1,78 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from '../event.service';
 import { Event } from '../event'
+import { Participant } from '../../participant/participant';
+import { ActivatedRoute } from '@angular/router';
 
 
 @Component({
   selector: 'event-list',
   templateUrl: './list.component.html',
-  styleUrl: './list.component.css'
+  styleUrl: './list.component.css',
 })
 export class ListComponent implements OnInit{
   events: Event[] = [];
 
-  constructor(private eventService: EventService) {}
-
-  ngOnInit(): void {
-    this.getEvents();
+  constructor(private eventService: EventService, private route: ActivatedRoute) {
+    this.route.params.subscribe(params => {
+      this.events = []
+      this.getEvents(params['mode']);
+    });
   }
 
-  getEvents(): void {
-    this.eventService.getEvents()
-    .subscribe(
-      
-      events => {
-        this.events = events;
-      }
-    );
+  ngOnInit(): void {
+
+  }
+
+  getEvents(mode: string = "last"): void {
+    if (mode != 'last' && mode != 'upcomming')
+    {
+      mode = 'last'
+    }
+    if(mode == 'last')
+    {
+      this.eventService.getEventsLast()
+      .subscribe(
+        events => {
+          this.events = events;
+        }
+      );
+    }
+    if(mode == 'upcomming')
+    {
+      this.eventService.getEventsNext()
+      .subscribe(
+        events => {
+          this.events = events;
+        }
+      );
+    }
+  }
+
+  getAbsent(partipants: Participant[]| undefined)
+  {
+    if (partipants)
+    {
+      return partipants.filter(o => o.participation === "ABS").length;
+    }
+    return 0;
+  }
+
+  getExcused(partipants: Participant[] | undefined)
+  {
+    if (partipants)
+    {
+      return partipants.filter(o => o.participation === "EXC").length;
+    }
+    return 0;
+  }
+
+  getPresent(partipants: Participant[] | undefined)
+  {
+    if (partipants)
+    {
+      return partipants.filter(o => o.participation === "PRE").length;
+    }
+    return 0;
   }
 }
