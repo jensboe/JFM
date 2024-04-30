978d2f7d2adbf8e8074e57aa6e7ff8ab629cbcd6
Jens Böckmann <jensboe@outlook.de>
2024-05-01 00:29:56 +0200

    Bootstrap the project using standalone APIs

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,11 +3,15 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 import { AuthService } from './auth.service';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { NgIf, AsyncPipe } from '@angular/common';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    standalone: true,
+    imports: [NgIf, RouterLink, RouterLinkActive, RouterOutlet, AsyncPipe]
 })
 export class AppComponent {
   title = 'Jugendfeuerwehr Anwesenheit';
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,8 +2,20 @@
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { FormsModule } from '@angular/forms';
+import { EventModule } from './app/event/event.module';
+import { AppRoutingModule } from './app/app-routing.module';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, AppRoutingModule, EventModule, FormsModule),
+        provideAnimationsAsync()
+    ]
+})
   .catch(err => console.error(err));
