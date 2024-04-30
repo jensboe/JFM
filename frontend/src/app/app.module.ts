1f72d53f49a26285e7d18ade2f990971508def28
Jens Böckmann <jensboe@outlook.de>
2024-05-01 00:26:08 +0200

    Remove unnecessary NgModule classes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,10 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { MaterialModule } from './material/material.module';
+
 import { EventModule } from './event/event.module';
-import { MemberModule } from './member/member.module';
-import { ParticipantModule } from './participant/participant.module';
+
+
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
 
@@ -15,15 +15,12 @@ import { FormsModule } from '@angular/forms';
 @NgModule({
     declarations: [AppComponent],
     imports: [
-        BrowserModule,
-        AppRoutingModule,
-        MaterialModule,
-        EventModule,
-        MemberModule,
-        ParticipantModule,
-        FormsModule,
-        LoginComponent
-    ],
+    BrowserModule,
+    AppRoutingModule,
+    EventModule,
+    FormsModule,
+    LoginComponent
+],
     providers: [
         provideAnimationsAsync()
     ],
--- frontend/src/app/event/event.module.ts
+++ frontend/src/app/event/event.module.ts
@@ -2,26 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule, NgFor } from '@angular/common';
 import { NewComponent } from './new/new.component';
 import { ListComponent } from './list/list.component';
-import { MaterialModule } from '../material/material.module';
+
 import { HttpClientModule } from '@angular/common/http';
 import { ParticipationComponent } from './participation/participation.component';
 import { EventRoutingModule } from './event-routing.module';
 import { FormsModule } from '@angular/forms';
-import { ParticipantModule } from '../participant/participant.module';
+
 
 
 @NgModule({
     imports: [
-        CommonModule,
-        NgFor,
-        MaterialModule,
-        HttpClientModule,
-        EventRoutingModule,
-        FormsModule,
-        ParticipantModule,
-        NewComponent,
-        ListComponent,
-        ParticipationComponent
-    ],
+    CommonModule,
+    NgFor,
+    HttpClientModule,
+    EventRoutingModule,
+    FormsModule,
+    NewComponent,
+    ListComponent,
+    ParticipationComponent
+],
 })
 export class EventModule { }
