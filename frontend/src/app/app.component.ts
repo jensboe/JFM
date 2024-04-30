abb295ff020e2be621a3f0ac5f72ac43d2d9caf5
Jens Böckmann <jensboe@outlook.de>
2024-05-01 00:32:54 +0200

    Add missing imports

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -5,13 +5,18 @@ import { map, shareReplay } from 'rxjs/operators';
 import { AuthService } from './auth.service';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { NgIf, AsyncPipe } from '@angular/common';
-
+import { MatSidenav, MatSidenavContent, MatSidenavContainer } from '@angular/material/sidenav';
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatIcon } from '@angular/material/icon';
+import { MatNavList } from '@angular/material/list';
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
     standalone: true,
-    imports: [NgIf, RouterLink, RouterLinkActive, RouterOutlet, AsyncPipe]
+    imports: [NgIf, RouterLink, RouterLinkActive, RouterOutlet, AsyncPipe,
+      MatSidenav, MatSidenavContent, MatSidenavContainer, MatToolbar, MatIcon, MatNavList
+    ]
 })
 export class AppComponent {
   title = 'Jugendfeuerwehr Anwesenheit';
