e39510a80ab83257ceced152180b9001f1689de7
Jens Böckmann <jensboe@outlook.de>
2024-05-05 15:07:11 +0200

    Cleaining

--- backend/events/models.py
+++ backend/events/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
-from django.urls import reverse
 from members.models import Member
 
 class Event(models.Model):
