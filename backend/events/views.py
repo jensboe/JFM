064c1c596d29c46df93fbb322ad08e16441ef7f5
Jens Böckmann <jensboe@outlook.de>
2024-05-11 19:21:10 +0200

    Prefetch participants

--- backend/events/views.py
+++ backend/events/views.py
@@ -31,8 +31,8 @@ class EventFilter(django_filters.rest_framework.FilterSet):
         fields = ['start_date', 'end_date']
 
 class EventViewSet(viewsets.ModelViewSet):
-    queryset = Event.objects.all()
-    # queryset = Event.objects.all().prefetch_related('participants')
+    # queryset = Event.objects.all()
+    queryset = Event.objects.all().prefetch_related('participants')
     serializer_class = EventSerializer
     filter_backends = [django_filters.rest_framework.DjangoFilterBackend,
                        filters.OrderingFilter]
