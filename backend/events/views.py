812a116b6d2b6c6fae071ab8e867640a731c64dd
Jens Böckmann <jensboe@outlook.de>
2025-01-26 19:27:34 +0100

    Add requirement_type to api

--- backend/events/views.py
+++ backend/events/views.py
@@ -23,7 +23,7 @@ class EventSerializer(serializers.ModelSerializer):
     participants = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
     class Meta:
         model = Event
-        fields = ['pk', 'title', 'start_date', 'end_date', 'participants']
+        fields = ['pk', 'title', 'start_date', 'end_date', 'requirement_type', 'participants']
 
 class EventFilter(django_filters.rest_framework.FilterSet):
     end_after = django_filters.rest_framework.IsoDateTimeFilter(field_name="end_date", lookup_expr='gte')
